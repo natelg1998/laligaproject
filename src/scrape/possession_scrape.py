b80372af84fa4ee17fafcee5459f7263c92858b9
natelg1998 <ntlg414@gmail.com>
2022-09-27 11:39:22 -0400

    Update scrapes to collect by season looping back through prior seasons

--- src/scrape/possession_scrape.py
+++ src/scrape/possession_scrape.py
@@ -6,30 +6,30 @@ import os
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from pprint import pprint
 
-years = ["2021-2022", "2020-2021", "2019-2020", "2019-2020", "2018-2019",
+years = ["2022-2023", "2021-2022", "2020-2021", "2019-2020", "2019-2020", "2018-2019",
          "2017-2018", "2016-2017", "2015-2016", "2014-2015", "2013-2014", "2012-2013",
          "2011-2012", "2010-2011", "2009-2010", "2008-2009", "2007-2008", "2006-2007",
          "2005-2006", "2004-2005", "2003-2004", "2002-2003", "2001-2002", "2000-2001"]
-
-
+# years = list(range(2022, , -1))
 url = "https://fbref.com/en/comps/12/La-Liga-Stats"
 
+
 def get_score_links():
-    data = requests.get(url)
-    soup = BeautifulSoup(data.text, "lxml")
-    league_table = soup.select("table.stats_table")[0]
-    squad_links = league_table.find_all('a')
-    squad_links = [l.get("href") for l in squad_links]
-    squad_links = [l for l in squad_links if "/en/squads" in l]
-    squad_links_year = [f"{l.rsplit('/',1)[0]}/{year}" for l in squad_links for year in years]
-    team_urls = [f"https://fbref.com{l}" for l in squad_links_year]
-    # team_ids = [l.rsplit("/", 1)[0] for l in team_urls]
-    # team_stats = [t.rsplit("/", 1)[-1] for t in team_urls]
-    # score_stats = []
-    # for team in team_ids:
-    #     for year in years:
-    #         score_stats.append(f"{team}/{year}")
-    return team_urls
+    global url
+    teams = []
+    for year in years:
+        data = requests.get(url)
+        soup = BeautifulSoup(data.text, "lxml")
+        league_table = soup.select("table.stats_table")[0]
+        squad_links = league_table.find_all('a')
+        squad_links = [l.get("href") for l in squad_links]
+        squad_links = [l for l in squad_links if "/en/squads" in l]
+        # squad_links_year = [f"{l.rsplit('/',1)[0]}/{year}/{l.rsplit('/',1)[-1]}" for l in squad_links for year in years]
+        team_urls = [f"https://fbref.com{l}" for l in squad_links]
+        previous_season = soup.select("a.prev")[0].get("href")
+        url = f"https://fbref.com{previous_season}"
+        teams.extend(team_urls)
+    return teams
 
 poss_l = []
 def get_defense_data(url):
@@ -63,7 +63,7 @@ def get_defense_data(url):
 if __name__ == "__main__":
     start = time.time()
     score_links = get_score_links()
-    with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers = 7) as executor:
         executor.map(get_defense_data, score_links)
     print(len(poss_l))
     poss_df = pd.concat(poss_l, ignore_index=True)
--- src/scrape/scores_overall_scrape.py
+++ src/scrape/scores_overall_scrape.py
@@ -10,38 +10,43 @@ years = ["2022-2023", "2021-2022", "2020-2021", "2019-2020", "2019-2020", "2018-
          "2017-2018", "2016-2017", "2015-2016", "2014-2015", "2013-2014", "2012-2013",
          "2011-2012", "2010-2011", "2009-2010", "2008-2009", "2007-2008", "2006-2007",
          "2005-2006", "2004-2005", "2003-2004", "2002-2003", "2001-2002", "2000-2001"]
-# years = list(range(2022, , -1))
+
 url = "https://fbref.com/en/comps/12/La-Liga-Stats"
 
 
 def get_score_links():
     global url
-    teams = []
+    seasons = []
     for year in years:
         data = requests.get(url)
         soup = BeautifulSoup(data.text, "lxml")
-        league_table = soup.select("table.stats_table")[0]
-        squad_links = league_table.find_all('a')
-        squad_links = [l.get("href") for l in squad_links]
-        squad_links = [l for l in squad_links if "/en/squads" in l]
-        # squad_links_year = [f"{l.rsplit('/',1)[0]}/{year}/{l.rsplit('/',1)[-1]}" for l in squad_links for year in years]
-        team_urls = [f"https://fbref.com{l}" for l in squad_links]
+        # https://stackoverflow.com/questions/42038130/beautifulsoup-nested-class-selector
+        scores_sel = soup.find(class_ = "inactive").find(class_ = "hoversmooth").find_all('a')
+        # scores_sel = scores_sel.find_all('a')
+        scores_links = [l.get("href") for l in scores_sel]
+        scores_links = [l for l in scores_links if "schedule" in l]
+        scores_links = [*set(scores_links)]
+        # league_table = soup.select("table.stats_table")[0]
+        # squad_links = league_table.find_all('a')
+        # squad_links = [l.get("href") for l in squad_links]
+        # squad_links = [l for l in squad_links if "/en/squads" in l]
+        # # squad_links_year = [f"{l.rsplit('/',1)[0]}/{year}/{l.rsplit('/',1)[-1]}" for l in squad_links for year in years]
+        team_urls = [f"https://fbref.com{l}" for l in scores_links]
         previous_season = soup.select("a.prev")[0].get("href")
         url = f"https://fbref.com{previous_season}"
-        teams.extend(team_urls)
-    return teams
-
+        seasons.extend(team_urls)
+    return seasons
 # pprint(get_score_links())
+
 scores = []
 def get_scores_fixtures(url):
     data = requests.get(url)
     matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
-    team_name = url.split("/")[-1].replace("-Stats", "").replace("-", " ")
-    matches["Team"] = team_name
+    # team_name = url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+    # matches["Team"] = team_name
     scores.append(matches)
     print(".", end="", flush=True)
     return
-
 
 if __name__ == "__main__":
     start = time.time()
@@ -50,12 +55,6 @@ if __name__ == "__main__":
         executor.map(get_scores_fixtures, score_links)
     print(len(scores))
     scores_df = pd.concat(scores, ignore_index = True)
-    scores_df.to_csv(os.path.join(os.path.realpath("./data/fbref_data/"), r"scores.csv"))
+    scores_df.to_csv(os.path.join(os.path.realpath("./data/fbref_data/"), r"scores_overall.csv"))
     end = time.time()
     print(f"Time taken to run: {end - start} seconds")
-# # Time taken to run: 2173.9899361133575 seconds - 25 workers
-# # Time taken to run: 1972.1386992931366 seconds - 6 workers
-# # Time taken to run: 5107.039206266403 seconds - 5 workers
-# # Time taken to run: 1563.7288415431976 seconds - 7 workers
-# # Time taken to run: 3590.750078201294 seconds - 8 workers
-# # Time taken to run: 1881.5751566886902 seconds - Default 12 workers
